b90ad64bd42454931fa6e809035bd3217a2e8971
jp <gonjuanp@gmail.com>
2013-06-10 20:52:20 -0500

    ver 1.2
    Spaces syntax created

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,13 +4,12 @@ class SessionController < ApplicationController
   end
 
   def create
-  session_user = params [:session]
+  session_user = params[:session]
   user= User.find_by_email(session_user[:email])
 
   
  if user and user.authenticate(session_user[:password])
-     session[:User_id]= User.id
-	session[:user_id] = user.id
+     session[:user_id]= user.id	
 	redirect_to root_url, notice: "logged in succesfully"
   else
     redirect_to login_url, alert: "Invalid user/password"  
